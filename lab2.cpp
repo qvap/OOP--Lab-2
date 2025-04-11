93e2e146d63defbce49479ea270b1f41974ae2c1
qvap <135361555+qvap@users.noreply.github.com>
2025-04-11 19:30:39 +0500

    New class Sections

--- lab2.cpp
+++ lab2.cpp
@@ -64,14 +64,44 @@ class ColoredPoint: public Point {
         };
 };
 
+class Section {
+    protected:
+        Point *p1;
+        Point *p2;
+    public:
+        Section(){
+            printf("Section()\n");
+            p1 = new Point;
+            p2 = new Point;
+        };
+        Section(int x1, int y1, int x2, int y2){
+            printf("Section(int x1, int y1, int x2, int y2)\n");
+            p1 = new Point(x1, y1);
+            p1 = new Point(x2, y2);
+        };
+        Section(const Section &s){
+            printf("Section(const Section &s)\n");
+            p1 = new Point(*(s.p1));
+            p2 = new Point(*(s.p2));
+        };
+        ~Section(){
+            delete p1;
+            delete p2;
+            printf("~Section()\n");
+        }
+};
+
 int main(){
     {
+        printf("\nStatic objects\n\n");
         // Статистически созданные объекты
         Point p;
         Point p2(10, 20);
         Point p3(p2);
         //
     };
+
+    printf("\nDynamic objects\n\n");
 
     // Динамически созданные объекты
     Point *p4 = new Point;
@@ -83,6 +113,8 @@ int main(){
     delete p6;
     //
 
+    printf("\nMove and reset object\n\n");
+
     // Перемещение и сброс объекта
     Point *p7 = new Point(1, 2);
     p7->move(10, 10);
@@ -91,9 +123,21 @@ int main(){
     delete p7;
     //
 
+    printf("\nInheritance\n\n");
+
     //Класс-наследник
     ColoredPoint *p8 = new ColoredPoint(1, 2, 42);
     delete p8;
+    //
+
+    printf("\nSections\n\n");
+
+    // Отрезки
+    Section *s1 = new Section;
+    Section *s2 = new Section(*s1);
+
+    delete s1;
+    delete s2;
     //
 
     system("pause");
