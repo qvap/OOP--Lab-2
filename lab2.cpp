7d7569299a092eecaa0a4f341b51356615582700
qvap <qvapelsin@gmail.com>
2025-04-22 10:43:29 +0500

    change printf to cout

--- lab2.cpp
+++ lab2.cpp
@@ -1,30 +1,31 @@
 #include <iostream>
+using namespace std;
 
 class Point {
     protected:
         int x, y;
     public:
         Point(){
-            printf("Point()\n");
+            cout << "Point()\n";
             x = 0;
             y = 0;
         };
         Point(int x, int y){
-            printf("Point(int x, int y)\n");
+            cout << "Point(int x, int y)\n";
             this->x = x;
             this->y = y;
         };
         Point(const Point &p){
-            printf("Point(const Point &p)\n");
+            cout << "Point(const Point &p)\n";
             x = p.x;
             y = p.y;
         };
         ~Point(){
-            printf("%d, %d\n", x, y);
-            printf("~Point()\n");
+            cout << x << " " << y << "\n";
+            cout << "~Point()\n";
         };
         void move(int dx, int dy){
-            printf("Point moved by %d, %d, new coordinates: %d, %d\n", dx, dy, x + dx, y + dy);
+            cout << "Point moved by " << dx << ", " << dy <<", new coordinates: " << x + dx << ", " << y + dy << "\n";
             x = x + dx;
             y = y + dy;
         };
@@ -32,7 +33,7 @@ class Point {
 };
 
 void Point::reset(){
-    printf("Reset Point\n");
+    cout << "Reset Point\n";
     x = 0;
     y = 0;
 };
@@ -42,22 +43,22 @@ class ColoredPoint: public Point {
         int color;
     public:
         ColoredPoint() : Point(){
-            printf("ColoredPoint()\n");
+            cout << "ColoredPoint()\n";
             color = 0;
         };
         ColoredPoint(int x, int y, int color) : Point(x, y){
-            printf("ColoredPoint(int x, int y, int color)\n");
+            cout << "ColoredPoint(int x, int y, int color)\n";
             this->color = color;
         };
         ColoredPoint(const ColoredPoint &p){
-            printf("ColoredPoint(const ColoredPoint &p)\n");
+            cout << "ColoredPoint(const ColoredPoint &p)\n";
             color = p.color;
             x = p.x;
             y = p.y;
         };
         ~ColoredPoint(){
-            printf("%d, %d, color = %d\n", x, y, color);
-            printf("~ColoredPoint()\n");
+            cout << x << ", " << y << ", color = " << color << "\n";
+            cout << "~ColoredPoint()\n";
         };
         void change_color(int new_color){
             color = new_color;
@@ -70,30 +71,30 @@ class Section {
         Point *p2;
     public:
         Section(){
-            printf("Section()\n");
+            cout << "Section()\n";
             p1 = new Point;
             p2 = new Point;
         };
         Section(int x1, int y1, int x2, int y2){
-            printf("Section(int x1, int y1, int x2, int y2)\n");
+            cout << "Section(int x1, int y1, int x2, int y2)\n";
             p1 = new Point(x1, y1);
             p2 = new Point(x2, y2);
         };
         Section(const Section &s){
-            printf("Section(const Section &s)\n");
+            cout << "Section(const Section &s)\n";
             p1 = new Point(*(s.p1));
             p2 = new Point(*(s.p2));
         };
         ~Section(){
             delete p1;
             delete p2;
-            printf("~Section()\n");
+            cout << "~Section()\n";
         }
 };
 
 int main(){
     {
-        printf("\nStatic objects\n\n");
+        cout << "\n---------Static objects---------\n\n";
         // Статистически созданные объекты
         Point p;
         Point p2(10, 20);
@@ -101,7 +102,7 @@ int main(){
         //
     };
 
-    printf("\nDynamic objects\n\n");
+    cout << "\n---------Dynamic objects---------\n\n";
 
     // Динамически созданные объекты
     Point *p4 = new Point;
@@ -113,7 +114,7 @@ int main(){
     delete p6;
     //
 
-    printf("\nMove and reset object\n\n");
+    cout << "\n---------Move and reset object---------\n\n";
 
     // Перемещение и сброс объекта
     Point *p7 = new Point(1, 2);
@@ -123,14 +124,14 @@ int main(){
     delete p7;
     //
 
-    printf("\nInheritance\n\n");
+    cout << "\n---------Inheritance---------\n\n";
 
     //Класс-наследник
     ColoredPoint *p8 = new ColoredPoint(1, 2, 42);
     delete p8;
     //
 
-    printf("\nSections\n\n");
+    cout << "\n---------Sections---------\n\n";
 
     // Отрезки
     Section *s1 = new Section;
